426a16a3bb7f45b4848666da5b2ea9db77543525
ngngochienvinh07-crypto <ngngochienvinh07@gmail.com>
2026-07-17 08:50:48 +0700

    Update database.py

--- ss18b3/database.py
+++ ss18b3/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:123456@localhost/course_management"
+DATABASE_URL = "mysql+pymysql://root:21072007@localhost/course_management"
 
 engine = create_engine(DATABASE_URL)
 
